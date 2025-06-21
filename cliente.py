aec69a618a2b1eeee2ed154c676946bf01c05c1b
Letícia Leitão <99267278+leticialevleitao@users.noreply.github.com>
2025-06-21 01:08:02 -0300

    add feature de conversão para txt
    - Conversão de mensagens em arquivos txt no cliente antes do envio
    - Mensagens são reconstruídas no servidor e salvas como txt
    - Lógica de fragmentação de pacotes até 1024 bytes
    - Uso de threading para receber mensagens em tempo real

--- cliente.py
+++ cliente.py
@@ -1,18 +1,94 @@
-from socket import *
-hostname=socket.gethostname()
-serverName=hostname
-serverPort=15000
-clienteSocket=socket(socket.AF_INET, socket.SOCK_DGRAM)##cria o socket do cliente
-#afnet--- ipv4
-#sockdgram----socket udp
-#porta do cliente e definida pelo so
-message=input('input sentence:')
-nome = input("Digite seu nome: ")
-hello = f"hi, meu nome eh {nome}"
+import socket
+import random
+import threading
+import math
+import struct
+from zlib import crc32
 
-clienteSocket.sendto(message,(serverName, serverPort))
+# Configuracoes do servidor
 
-modifiedMessage,serverAddress=clienteSocket.recvfrom(1024)
-print (modifiedMessage)# imprime pro usuario
-clienteSocket.close()
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 12000
+BUFF_SIZE = 1024
 
+# Cria o socket UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind((SERVER_IP, random.randint(1000, 9998)))
+
+
+# Funcao para receber mensagens
+
+def receive():
+    while True:
+        try:
+            message, _ = client.recvfrom(BUFF_SIZE)
+            print(message.decode(encoding="ISO-8859-1"))
+        except:
+            pass
+
+
+# Funcao para criar um arquivo .txt com a mensagem
+
+def convert_string_to_txt(nickname, message):
+    filename = f"{nickname}.txt"
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(message)
+    return filename
+
+
+# Funcao para criar um fragmento com header
+
+def create_fragment(contents, frag_size, frag_index, frag_count):
+    data = bytearray()
+    data.extend(contents[:frag_size])
+    crc = crc32(data)
+    header = struct.pack('!IIII', frag_size, frag_index, frag_count, crc)
+    return header + data
+
+
+# Inicia a thread de recebimento
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+
+# Loop principal
+
+is_conected = False
+
+while True:
+    message = input()
+    client_ip = client.getsockname()[0]
+
+    if message.startswith("hi, meu nome eh "):
+        if is_conected:
+            print("Você já está conectado à sala!")
+        else:
+            nickname = message[16:]
+            is_conected = True
+            client.sendto(f"SIGNUP_TAG:{nickname}".encode(), (SERVER_IP, SERVER_PORT))
+
+    elif message == "bye":
+        if not is_conected:
+            print("Você nao está conectado à sala!")
+        else:
+            client.sendto(f"QUIT_TAG:{nickname}".encode(), (SERVER_IP, SERVER_PORT))
+            is_conected = False
+
+    elif is_conected:
+        path_file = convert_string_to_txt(nickname, message)
+        with open(path_file, "rb") as file:
+            contents = file.read()
+
+        frag_size = BUFF_SIZE - 16
+        frag_index = 0
+        frag_count = math.ceil(len(contents) / frag_size)
+
+        while contents:
+            fragment = create_fragment(contents, frag_size, frag_index, frag_count)
+            client.sendto(fragment, (SERVER_IP, SERVER_PORT))
+            contents = contents[frag_size:]
+            frag_index += 1
+
+    else:
+        print("Comando inválido!")
--- server.py
+++ server.py
@@ -1,12 +1,143 @@
-from socket import *
-serverPort=15000
-hostname=socket.gethostname()
+import socket
+import queue
+import threading
+import struct
+from datetime import datetime
 
-serverSocket=socket(AF_INET,SOCK_DGRAM)
-serverSocket.bind((hostname, serverPort))
-print ("the server is readyyyy")
-while 1:
-    message=clientAdress=serverSocket.recvfrom(1024)###espera uma mensagem chegarr
 
-    modifiedMessage=message.upper()##aqui é comp trata a e oque manda pro cliente de volta
-    serverSocket.sendto(modifiedMessage,clientAdress)
+# Configuracoes do servidor
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 12000
+
+
+BUFF_SIZE = 1024
+
+
+# Inicializacao
+
+messages = queue.Queue()
+clients_ip = []
+clients_nickname = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((SERVER_IP, SERVER_PORT))
+
+
+# Funcao para salvar mensagem em .txt
+
+def convert_string_to_txt(nickname, message):
+    filename = f"{nickname}_server.txt"
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(message)
+    return filename
+
+
+# Funcao para pegar data e hora atual
+
+def get_current_time_and_date():
+    now = datetime.now()
+    return now.strftime("%H:%M:%S %d/%m/%Y")
+
+
+# Funcao para remover cliente
+
+def remove_client(client):
+    index_client = clients_ip.index(client)
+    clients_ip.remove(client)
+    clients_nickname.pop(index_client)
+
+
+# Thread para receber mensagens
+
+def receive():
+    received_chunks = 0
+    rec_list = []
+
+    while True:
+        try:
+            message_received_bytes, address_ip_client = server.recvfrom(BUFF_SIZE)
+
+            if (message_received_bytes.decode().startswith("SIGNUP_TAG:") or
+                message_received_bytes.decode().startswith("QUIT_TAG:")):
+                messages.put((message_received_bytes, address_ip_client))
+            else:
+                raise NameError('MESSAGE_TAG')
+
+        except:
+            header = message_received_bytes[:16]
+            message_received_bytes = message_received_bytes[16:]
+            (frag_size, frag_index, frag_count, crc) = struct.unpack('!IIII', header)
+
+            if len(rec_list) < frag_count:
+                need_to_add = frag_count - len(rec_list)
+                rec_list.extend([''] * need_to_add)
+
+            rec_list[frag_index] = message_received_bytes
+            received_chunks += 1
+
+            if received_chunks == frag_count:
+                for ip in clients_ip:
+                    if ip == address_ip_client:
+                        index = clients_ip.index(ip)
+                        name = clients_nickname[index]
+                        break
+
+                content = b''.join(rec_list)
+                content = content.decode(encoding="ISO-8859-1")
+                path_file = convert_string_to_txt(name, content)
+
+                with open(path_file, "r") as arquivo:
+                    message = f"{name}: {arquivo.read()}".encode()
+
+                messages.put((message, address_ip_client))
+
+                received_chunks = 0
+                rec_list = []
+
+            elif (received_chunks < frag_count) and (frag_index == frag_count - 1):
+                print("Houve perda de pacote!")
+                received_chunks = 0
+                rec_list = []
+
+
+# Thread para enviar mensagens
+
+def broadcast():
+    while True:
+        while not messages.empty():
+            message_bytes, address_ip_client = messages.get()
+            decoded_message = message_bytes.decode(encoding="ISO-8859-1")
+
+            if address_ip_client not in clients_ip:
+                name = decoded_message[decoded_message.index(":")+1:]
+                clients_ip.append(address_ip_client)
+                clients_nickname.append(name)
+
+            for client_ip in clients_ip:
+                try:
+                    if decoded_message.startswith("SIGNUP_TAG:"):
+                        nickname = decoded_message[decoded_message.index(":")+1:]
+                        server.sendto(f"{nickname} se juntou ".encode(), client_ip)
+
+                    elif decoded_message.startswith("QUIT_TAG:"):
+                        nickname = decoded_message[decoded_message.index(":")+1:]
+                        remove_client(client_ip)
+                        server.sendto(f"{nickname} saiu da sala!".encode(), client_ip)
+
+                    else:
+                        ip = address_ip_client[0]
+                        port = address_ip_client[1]
+                        message_output = f'{ip}:{port}/~{decoded_message} {get_current_time_and_date()}'
+                        server.sendto(message_output.encode(encoding='ISO-8859-1'), client_ip)
+
+                except:
+                    remove_client(client_ip)
+
+
+# Inicia as threads
+
+receive_tread = threading.Thread(target=receive)
+broadcast_tread = threading.Thread(target=broadcast)
+
+receive_tread.start()
+broadcast_tread.start()
