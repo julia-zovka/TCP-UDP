1c2a13b63c880c4502d5f79cf3990dadf5922b64
julia-zovka <jzs@cin.ufpe.com.br>
2025-08-05 17:18:38 -0300

    cecksum

--- Entrega-2-RDT_3.0/utils/create_frag.py
+++ Entrega-2-RDT_3.0/utils/create_frag.py
@@ -1,17 +1,21 @@
-
 import struct
-from zlib import crc32
 from utils.checksum import find_checksum
 
 
 def create_fragment(contents, frag_size, frag_index, frag_count):
-    # Calcula a posição inicial e final do fragmento
+     # Calcula a posição inicial e final do fragmento
     start = frag_index * frag_size
     end = start + frag_size
     fragment_data = contents[start:end]
     actual_size = len(fragment_data)
-    # Calcula CRC apenas dos dados do fragmento
-    crc = crc32(fragment_data)
+
+    header_no_checksum = struct.pack('!IIIII', actual_size, frag_index, frag_count, seq_num, ack_num)
+
+    # calcula o checksum 
+    data_for_checksum = header_no_checksum + fragment_data
+    checksum_check = find_checksum(data_for_checksum)
+    checksum_int = int(checksum_check, 2)
+
     # Monta o cabeçalho (tamanho real, índice, total, crc)
-    header = struct.pack('!IIII', actual_size, frag_index, frag_count, crc)
+    header = struct.pack('!IIII', actual_size, frag_index, frag_count, seq_num, ack_num, checksum_int)
     return header + fragment_data
