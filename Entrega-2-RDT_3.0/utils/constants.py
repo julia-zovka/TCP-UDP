0d060b291719f53fdaf8530c93066bf5d44a6717
julia-zovka <jzs@cin.ufpe.com.br>
2025-08-05 16:37:19 -0300

    novas variaveis

--- Entrega-2-RDT_3.0/utils/constants.py
+++ Entrega-2-RDT_3.0/utils/constants.py
@@ -1,8 +1,15 @@
 # Configuracoes do servidor
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 12000
+
+SERVER_ADRR = ("127.0.0.1", 12000)
+
 BUFF_SIZE        = 1024
 HEADER_SIZE      = 24
 # max do payload em cada fragmento
 FRAG_SIZE        = BUFF_SIZE - HEADER_SIZE
 DATA_FOLDER_PATH = "./Entrega-2-RDT_3.0/data_folder"
+
+
+TIMEOUT = 0.5
+#ACK_RECEIVED = False
