6483cb50f8f03d6fe76edac220013ae570984c37
julia-zovka <jzs@cin.ufpe.br>
2025-08-06 20:40:24 -0300

    precsiso mudar clientes

--- Entrega-2-RDT_3.0/cliente.py
+++ Entrega-2-RDT_3.0/cliente.py
@@ -8,15 +8,14 @@ from zlib import crc32
 
 from utils.checksum import find_checksum
 from utils.sending_pkts import send_packet
-from utils.create_frag import  create_fragment
-import utils.variables as g 
+import utils.constants as g 
 
 
 # Configuracoes do servidor
 
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 12000
-BUFF_SIZE = 1024
+SERVER_IP =g.SERVER_IP
+SERVER_PORT =g.SERVER_PORT
+BUFF_SIZE=g.BUFF_SIZE
 
 # Cria o socket  e atribi uma porta aleatória a ele
 
@@ -26,8 +25,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind((SERVER_IP, random.randint(1000, 9998)))
 
 ##### variabeis globais
-seq_num_client = 0 # Número de sequência do pacote enviado pelo cliente
-ack_to_send = 0 # Número de reconhecimento do pacote enviado pelo servidor 
+seq_num_client = 0 # Número de sequência para enviar pacote  pelo cliente
+ack_expected = 0 # Número de reconhecimento do pacote esperado no  servidor 
 client_ip = None 
 nickname = None
 message_buffer = ''
@@ -50,6 +49,7 @@ def apresentacao():
 
 def receive():
     buffer = {}
+    global ack_to_send
     
     while True:
         try:
@@ -162,3 +162,97 @@ while True:
 
     else:
         print("Comando inválido!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import socket
+import struct
+import threading
+from utils.sending_pkts import send_packet
+from utils.checksum import find_checksum
+from utils.constants import SERVER_IP, SERVER_PORT, BUFF_SIZE, HEADER_SIZE
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind((SERVER_IP, 0))  # Usa uma porta aleatória
+
+seq_num_client = 0  # número de sequência para envio
+ack_expected = 0    # número de sequência esperado do servidor
+
+client_ip = client.getsockname()[0]
+nickname = input("Digite seu nickname: ").strip()
+
+print("\nPara entrar na sala, digite:\n  hi, meu nome eh <nickname>")
+print("Para sair, digite:\n  bye\n")
+
+# Função para receber mensagens
+def receive():
+    global ack_expected
+    buffer = {}
+    
+    while True:
+        try:
+            data, _ = client.recvfrom(BUFF_SIZE)
+            header = data[:HEADER_SIZE]
+            fragment = data[HEADER_SIZE:]
+
+            frag_size, frag_index, frag_count, seq_num, ack_num, checksum = struct.unpack('!IIIIII', header)
+
+            header_no_checksum = struct.pack('!IIIII', frag_size, frag_index, frag_count, seq_num, ack_num)
+            data_for_checksum = header_no_checksum + fragment
+            checksum_check = find_checksum(data_for_checksum)
+
+            if checksum != checksum_check or seq_num != ack_expected:
+                print("[ERRO] Pacote corrompido ou fora de ordem, reenviando ACK anterior")
+                send_packet('', client, (SERVER_IP, SERVER_PORT), client_ip, nickname, seq_num_client, 1 - ack_expected)
+                continue
+
+            # Monta o buffer por fragmento
+            if "frags" not in buffer:
+                buffer["frags"] = [None] * frag_count
+                buffer["recebidos"] = 0
+
+            if buffer["frags"][frag_index] is None:
+                buffer["frags"][frag_index] = fragment
+                buffer["recebidos"] += 1
+
+            # Se completou todos os fragmentos
+            if buffer["recebidos"] == frag_count:
+                msg = b''.join(buffer["frags"]).decode("utf-8", errors="ignore")
+                print(msg)
+                send_packet('', client, (SERVER_IP, SERVER_PORT), client_ip, nickname, seq_num_client, ack_expected)
+                ack_expected = 1 - ack_expected
+                buffer.clear()
+
+        except Exception as e:
+            print(f"[ERRO] {e}")
+
+# Thread para receber mensagens
+threading.Thread(target=receive, daemon=True).start()
+
+# Loop principal de envio
+while True:
+    try:
+        msg = input()
+        if msg.strip() == "":
+            continue
+        send_packet(msg, client, (SERVER_IP, SERVER_PORT), client_ip, nickname, seq_num_client, ack_expected)
+        seq_num_client = 1 - seq_num_client
+    except KeyboardInterrupt:
+        print("Encerrando cliente.")
+        break
